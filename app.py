40504f6f83db52ea06007490f1f95a42afe1df53
Pratibhaaa12 <87256006+Pratibhaaa12@users.noreply.github.com>
2025-04-30 02:55:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-# Replace with your MySQL credentials
+     
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Sector51@localhost/book_authordb'
 
 
